46c7780d0576e4ee6b13bc14e64f3182096af368
isisestevan <isisestevan@gmail.com>
2021-02-10 16:13:14 -0300

    Create fatoracao.c

---
+++ fatoracao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int num,result;
+    printf("Escolha um numero para fatorar\n");
+    scanf("%i",&num);
+    for(;num>=1;num--)
+    {
+        result=result*1;
+    } 
+    printf("%i",result);
+} 
